8fc85e90edf0ff3735c14f514f8c6e753a50f786
izzisolomon <isolomon@mshanken.com>
2018-01-22 11:50:37 -0500

    fixed ios icons

--- components/home.js
+++ components/home.js
@@ -1,27 +1,33 @@
 import React from "react";
 import { View, Text } from "react-native";
-import Icon from "react-native-vector-icons/MaterialIcons";
+import Icon from "react-native-vector-icons/Ionicons";
 
 export default class HomeScreen extends React.Component {
-  static navigationOptions = ({ navigation }) => ({
-    title: "Top Looks",
-    headerLeft: (
-      <View style={{ paddingLeft: 15 }}>
-        <Icon
-          size={20}
-          name={"menu"}
-          color="white"
-          onPress={() => navigation.navigate("DrawerOpen")}
-        />
-      </View>
-    )
-  });
+    static navigationOptions = ({ navigation }) => ({
+        title: "Top Looks",
+        headerLeft: (
+            <View style={{ paddingLeft: 15 }}>
+                <Icon
+                    size={20}
+                    name={"ios-menu"}
+                    color="white"
+                    onPress={() => navigation.navigate("DrawerOpen")}
+                />
+            </View>
+        )
+    });
 
-  render() {
-    return (
-      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Home Screen</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: "center",
+                    justifyContent: "center"
+                }}
+            >
+                <Text>Home Screen</Text>
+            </View>
+        );
+    }
 }
